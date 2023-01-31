628d4ba5ee90d7d7542683fce6177331464c51e2
DochaPutina <arten.smernov@bk.ru>
2023-01-31 13:28:08 +0300

    second commit with some exceptions

---
+++ src/main/java/ServingWebContentApplication.java
@@ -0,0 +1,32 @@
+import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import service.UsersService;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/users")
+public class ServingWebContentApplication {
+
+    @Autowired
+    private UsersService service;
+
+    @PostMapping
+    public String addUser(@RequestBody Users users){
+        service.insertUsers(users);
+        return "user added";
+    }
+
+    @GetMapping
+    public List<Users> getUsers(){
+        return service.getUsers();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+}
--- src/main/java/service/UsersService.java
+++ src/main/java/service/UsersService.java
@@ -1,9 +1,12 @@
 package service;
 
 
-import com.tej.JooQDemo.jooq.sample.model.tables.Users;
+import com.tej.JooQDemo.jooq.sample.model.Tables;
+import com.tej.JooQDemo.jooq.sample.model.tables.pojos.Users;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsersService {
@@ -12,7 +15,24 @@ public class UsersService {
 
 
     public void insertUsers(Users users){
+        dslContext.insertInto(Tables.USERS, Tables.USERS.ID,
+                                            Tables.USERS.STATUSID,
+                                            Tables.USERS.EMAIL,
+                                            Tables.USERS.MONEY,
+                                            Tables.USERS.USERNAME,
+                                            Tables.USERS.PASSWORD,
+                                            Tables.USERS.NUMBER).values(users.getId(),
+                                                                        users.getStatusid(),
+                                                                        users.getEmail(),
+                                                                        users.getMoney(),
+                                                                        users.getUsername(),
+                                                                        users.getPassword(),
+                                                                        users.getNumber()).execute();
+    }
 
+    public List<Users> getUsers(){
+        return dslContext.selectFrom(Tables.USERS)
+                .fetchInto(Users.class);
     }
 
 }
