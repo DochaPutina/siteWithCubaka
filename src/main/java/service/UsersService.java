e0fcdc6b9d6163d304aea3b0d63ae2f7ba5b1157
DochaPutina <arten.smernov@bk.ru>
2023-01-31 12:33:01 +0300

    first commit

---
+++ src/main/java/service/UsersService.java
@@ -0,0 +1,18 @@
+package service;
+
+
+import com.tej.JooQDemo.jooq.sample.model.tables.Users;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    private DSLContext dslContext;
+
+
+    public void insertUsers(Users users){
+
+    }
+
+}
